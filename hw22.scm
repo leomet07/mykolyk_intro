56b14069a88739d95516c847a5da139ef03873fe
leomet07 <leomet07@gmail.com>
2022-10-25 20:40:51 -0400

    rename hw21 to hw21

--- hw22.scm
+++ hw22.scm
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname hw21) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname hw22) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;IntroCS pd4 sec4
 ;Team name: The Rear End
 ;collaborated with team
