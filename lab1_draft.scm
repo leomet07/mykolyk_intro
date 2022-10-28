bfbe22bc9a8321bf2683debc737125934bd935a4
leomet07 <leomet07@gmail.com>
2022-10-27 21:53:55 -0400

    lab 1 main coding done

--- lab1_draft.scm
+++ lab1_draft.scm
@@ -5,12 +5,6 @@
   (lambda (n g)
      (/(+ g (/ n g)) 2)))
 
-;(sqrt 7) "...real ^^^"
-
-;(foo 9 2)
-;(foo 9 3)
-;(foo 9 4)
-
 (define epsilon 0.1)
 
 (define isGoodEnuf?
@@ -27,7 +21,8 @@
   (lambda (n)
     (mySqrtHelper n (/ n 3))))
 
-"lenny sqrt"
-(mySqrt 169)
-
-
+(mySqrt 169) "...should be ~13"
+(mySqrt 1234) "...should be ~35.1"
+(mySqrt 4) "...should be ~2"
+(mySqrt 16) "...should be ~4"
+(mySqrt 144) "...should be ~12"
