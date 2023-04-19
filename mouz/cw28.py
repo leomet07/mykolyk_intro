a537c5bcf320c9aebd2d9fef2ef5877e6bfa24af
leomet07 <leomet07@gmail.com>
2023-04-19 13:49:46 -0400

    output data in cw28

--- mouz/cw28.py
+++ mouz/cw28.py
@@ -7,18 +7,34 @@ data = data.strip().split("\n")
 
 def studentAverage(student : str):
     items = student.split()
+    # Only keep numbers aka the grades
     grades = [float(item) for item in items if item.isdigit()]
     return sum(grades) / len(grades) 
 
 def studentAverageDropLowest(student : str):
     items = student.split()
-    grades = [float(item) for item in items if item.isdigit()]
+    # Only keep numbers aka the grades
+    grades = [float(item) for item in items if item.isdigit()] 
     grades.remove(min(grades))
     return sum(grades) / len(grades) 
 
 def compareAverages(averages : list):
+    calculated_avgs = []
     for student_data in averages:
         name = student_data.split()[1]
-        print(f"Student {name}, avg: {studentAverage(student_data)}, dropped_lowest: {studentAverageDropLowest(student_data)}")
+        avg = studentAverage(student_data)
+        dropped = studentAverageDropLowest(student_data)
+        print(f"Student {name}, avg: {avg}, dropped_lowest: {dropped}")
+        calculated_avgs.append([avg, dropped])
+    return calculated_avgs
 
-compareAverages(data)
+calculated_avgs = compareAverages(data)
+
+# Write the computed data
+with open("cw28output.txt", "w") as file_obj:
+    for i in range(len(data)):
+        student_data = data[i].split()
+        calculated_avg = [str(x) for x in calculated_avgs[i]]
+        student_data = student_data[:2]
+        student_data.extend(calculated_avg)
+        file_obj.write(" ".join(student_data) + "\n")
