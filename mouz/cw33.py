eed15f4aa71405a4a60a0b3e612276ac3dccdc2b
leomet07 <leomet07@gmail.com>
2023-05-08 10:13:56 -0400

    tesselations project

--- mouz/cw33.py
+++ mouz/cw33.py
@@ -1,51 +1,37 @@
 import turtle
-import math
 
 tess = turtle.Turtle()
 tess.pensize(2)
 turtle.tracer(False)
 
-def move_turtle(x : float, y : float):
-    tess.penup()
-    tess.home()
-    tess.setx(x)
-    tess.sety(y)
-    tess.pendown()
+# All 
+tess.setx(-200)
+tess.sety(400)
+size = 40
+for i in range(0, 5):
 
-def draw_rect(x : float, y : float, w : float, h : float):
-    move_turtle(x , y)
-    for i in range(4):
-        if i % 2 == 0:
-            tess.forward(w)
-        else:
-            tess.forward(h)
-        tess.left(90)
+    for i in range(25):
+        # Each quadrant
+        for i in range(20):
+            # Each row
+            tess.pendown()  
+            tess.pencolor(0.2, 0.55, 0.55)
+            
+            for i in range(4):
+                tess.forward(size)
+                tess.right(90)
+            for i in range(3):
+                tess.forward(size)
+                tess.right(120)
+            tess.forward(size)
+        tess.right(90)
+        tess.forward(size)
+        tess.right(90)
+        tess.forward(size * 20)
+        tess.left(180)
+    # tess.home()
+    tess.right(90 * i)
 
-
-
-def draw_circle(x : float, y : float, radius : float):
-    move_turtle(x , y)
-    # Forward 1: rad 57.2957795130823208768
-    # Forward 2: rad 114.5915590261646417536
-    forward = (radius * math.pi * 2) / 360
-    for i in range(480):
-        tess.forward(forward)
-        tess.right(1)
-        
-
-def draw_bus(x : float, y : float, size : float):
-    w = size * 10
-    h = size * 5
-    draw_rect(x, y, w, h)
-    draw_circle(x + size, y + size, size)
-    draw_circle(x + w - size, y + size, size)
-
-# draw_bus()
-# draw_circle(-250, -25, 200)
-# tess.setheading(0)
-# draw_rect(69, 69, 300, 90)
-tess.penup()
-draw_bus(50, 50, 30)
 turtle.update()
 
 turtle.mainloop()
