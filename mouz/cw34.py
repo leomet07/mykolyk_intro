6b828eee032f5ea4948037c754ef6a6575fed09d
leomet07 <leomet07@gmail.com>
2023-05-08 14:50:18 -0400

    cw34

---
+++ mouz/cw34.py
@@ -0,0 +1,37 @@
+import turtle as t
+
+t.tracer(False)
+def koch(t, order, size):
+    if order == 0: # The base case is just a straight line
+        t.forward(size)
+    else:
+        koch(t, order-1, size/3) # Go 1/3 of the way
+        t.left(60)
+        koch(t, order-1, size/3)
+        t.right(120)
+        koch(t, order-1, size/3)
+        t.left(60)
+        koch(t, order-1, size/3)
+
+def snow_flake(level : int, size : int):
+    for i in range(3):
+
+        t.pencolor(["red", "green", "blue"][i])
+        koch(t, level, size)
+        t.right(120)
+
+
+
+for i in range(3, -1, -1):
+    size = 70 * ((i ** 1.5) + 1)
+    level = i
+    t.penup()
+    t.home()
+    t.goto(size / -2, size / 3 )
+    t.pendown()
+    snow_flake(level, size)
+    t.penup()
+
+t.home()
+t.update()
+t.mainloop()
