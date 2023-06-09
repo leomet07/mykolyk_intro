361b21139c776a27407db30c8464c1c10b1d752f
leomet07 <leomet07@gmail.com>
2023-06-09 13:58:26 -0400

    cw44

---
+++ mouz/cw44/cw44.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import cgi
+import random
+
+def htmlFile():
+    with open("page.html") as file_obj:
+        return file_obj.read().strip()
+
+def htmlTop():
+    print("Content-Type: text/html\n\n")
+
+def getData():
+       
+    formData = cgi.FieldStorage()
+    firstname = formData.getvalue("first_name")
+    return firstname
+        
+ 
+
+def main():
+    htmlTop()
+    print(htmlFile())
+    #print('Hello {}'.format(getData()))
+    #print('Random number {}'.format(random.randint(1,100)))
+    #htmlTail()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        cgi.print_exception()
---
+++ mouz/cw44/submit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# Import modules for CGI handling 
+import cgi
+from cw44 import htmlTop
+# Create instance of FieldStorage 
+form = cgi.FieldStorage() 
+
+# Get data from fields
+if form.getvalue('drink') != None:
+   subject = form.getvalue('drink')
+else:
+   subject = "Not set"
+
+htmlTop()
+print(f"Drink is {subject}, form keys are {form.keys()}")
